cc934d9b835436dc4291982a9c7bb682c58e7649
isherezahin <abuzahinmohammadnowsin@gmail.com>
2024-09-05 16:03:19 +0600

    update

--- src/components/PostMessage.vue
+++ src/components/PostMessage.vue
@@ -3,7 +3,11 @@
     
     <div class="custom-container">
       <div class="custom-header">
-        <!-- SVG and Header Content -->
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin size-4" style="transform: rotate(30deg);">
+          <line x1="12" x2="12" y1="17" y2="22"></line>
+          <path d="M5 17h14v-1.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V6h1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4h1v4.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24Z"></path>
+        </svg>
+        Pinned
       </div>
       <div>Thank you for visiting my website. I would greatly appreciate any feedback you might have on my work. Please feel free to leave a comment. Thank you!</div>
     </div>
@@ -34,7 +38,7 @@
     <div class="comments-list">
       <div v-for="message in messages" :key="message.id" class="comment">
         <div class="comment-header">
-          <img class="avatar" :src="message.gender === 'man' ? '/manavatar.jpg' : '/womanavatar.jpg'" alt="User Avatar">
+          <img class="avatar" :src="message.gender === 'man' ? '../../public/manavatar.jpg' : '../../public/womanavatar.jpg'" alt="User Avatar">
           <div class="comment-info">
             <span class="comment-author">
               {{ message.name }}
